f93903e8d258875198951885decbf81ab74dc7a1
amacgowan <allmacg@yahoo.com>
2016-01-13 19:52:06 -0500

    Fit training data and compute accuracy for decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,10 +20,33 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+print "Number of features in training set is", len(features_train[0])
 
 #########################################################
-### your code goes here ###
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+scount = 0
+ccount = 0
+for label in pred:
+    if label == 0:
+        scount = scount + 1
+    else:
+        ccount = ccount + 1
+        
+print "Number of predicted emails by Sara is", scount
+print "Number of predicted emails by Chris is", ccount
+
+accuracy = clf.score(features_test, labels_test)
+
+print "accuracy of decision tree is", accuracy
+
+
+
 
 
 #########################################################
