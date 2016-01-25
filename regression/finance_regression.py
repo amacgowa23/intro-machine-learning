01fda1788b9348f1764253d82e7c0f629d7eaf2d
amacgowan <allmacg@yahoo.com>
2016-01-25 11:52:22 -0500

    Perform regression on Enron bonuses based on salary

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -19,9 +19,10 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
+
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"]  # could also have ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,6 +38,22 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+
+reg.fit(feature_train, target_train)
+
+slope = reg.coef_
+print "slope is", slope
+
+intcpt = reg.intercept_
+print "intercept is", intcpt
+
+trainr2 = reg.score(feature_train, target_train)
+print "r^2 value for training data is", trainr2
+
+testr2 = reg.score(feature_test, target_test)
+print "r^2 value for test data is", testr2
 
 
 
